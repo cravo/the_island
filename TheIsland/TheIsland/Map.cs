f0054adcf4ac565076b7b312b41a4488d760cf9b
Matt Craven <matthew.craven@btinternet.com>
2017-02-26 21:31:40 +0000

    Added Map class and Unit Test project with a couple of basic map tests

---
+++ TheIsland/TheIsland/Map.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TheIsland
+{
+    public class Map
+    {
+        public int Width { get; private set; }
+        public int Height { get; private set; }
+        public float GenerationProgress { get; private set; }
+        public bool Generated { get; private set; }
+
+        public Map(int width, int height)
+        {
+            Width = width;
+            Height = height;
+            GenerationProgress = 0;
+            Generated = false;
+        }
+
+        public void BeginGeneration()
+        {
+            ThreadPool.QueueUserWorkItem(Generate, null);
+        }
+
+        void Generate(object o)
+        {
+            Generated = false;
+
+            for(int y = 0; y < Height; ++y)
+            {
+                for(int x = 0; x < Width; ++x )
+                {
+                    int index = x + (y * Width);
+                    GenerationProgress = (float)index / (float)(Width * Height);
+                }
+            }
+
+            GenerationProgress = 1.0f;
+            Generated = true;
+        }
+    }
+}
---
+++ TheIsland/TheIslandTests/MapTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TheIsland;
+
+namespace TheIslandTests
+{
+    [TestClass]
+    public class MapTests
+    {
+        [TestMethod]
+        public void TestConstructionIsntGenerated()
+        {
+            Map map = new Map(10, 10);
+            Assert.AreEqual(map.Generated, false);
+        }
+
+        [TestMethod]
+        [Timeout(30000)]    // Assumes map generates within 30 seconds
+        public void TestMapGenerateSucceeds()
+        {
+            Map map = new Map(4096, 4096);
+
+            map.BeginGeneration();
+
+            while(!map.Generated)
+            {
+
+            }
+
+            Assert.AreEqual(map.Generated, true);
+            Assert.AreEqual(map.GenerationProgress, 1.0f);
+        }
+    }
+}
